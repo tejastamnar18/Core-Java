4289eda7e7ed33aa3c91322a847b84e0b3d9cad6
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-31 11:57:52 +0000

    method recursion

--- 01_Basic/method.java
+++ 01_Basic/method.java
@@ -1,10 +1,18 @@
 class method {
+    // public static void tejas(){
+    //     int a = 10;
+    //     int b= 20;
+    //     int c = a+b;
+    //     System.out.println(c);
+    // }
     public static void tejas(){
         int a = 10;
         int b= 20;
         int c = a+b;
         System.out.println(c);
+        // tejas();//!Method Recursion
     }
+    
     public static void main(String[] args){
         tejas();
     }
